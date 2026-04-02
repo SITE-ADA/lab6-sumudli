be66f0ad5464606dfbde76a44fe935067552c74a
sumudli <umudliyevs@gmail.com>
2026-04-02 23:06:18 +0400

    Add DTOs

---
+++ src/main/java/az/edu/ada/wm2/lab6/model/dto/CategoryRequestDto.java
@@ -0,0 +1,14 @@
+package az.edu.ada.wm2.lab6.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CategoryRequestDto {
+    private String name;
+}
